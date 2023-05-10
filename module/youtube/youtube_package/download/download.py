f797466c20c9fe1560c92caeeeefe9015f143d4c
takoyaki127 <s826062@gmail.com>
2023-05-10 22:51:14 +0900

    downloadの引数を変更

--- module/youtube/youtube_package/download/download.py
+++ module/youtube/youtube_package/download/download.py
@@ -2,15 +2,13 @@ from pytube.query import StreamQuery
 
 
 class Download():
-    def __init__(self, media: StreamQuery, itag, output, filename):
+    def __init__(self, media: StreamQuery, output):
         self.media = media
-        self.itag = itag
         self.output = output
-        self.filename = filename
 
     # ダウンロード開始
-    def download(self):
-        self.media.get_by_itag(self.itag).download(
+    def download(self,itag,filename):
+        self.media.get_by_itag(itag).download(
             output_path=self.output,
-            filename=self.filename
+            filename=filename
         )

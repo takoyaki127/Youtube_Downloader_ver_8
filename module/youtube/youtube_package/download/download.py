878e72887dc4430f795ade1002809792b7fe14ea
takoyaki127 <s826062@gmail.com>
2023-05-13 16:48:38 +0900

    変数の可視性を変更

--- module/youtube/youtube_package/download/download.py
+++ module/youtube/youtube_package/download/download.py
@@ -2,12 +2,12 @@ from pytube.query import StreamQuery
 
 
 class Download():
-    def __init__(self, media: StreamQuery, output):
-        self.media = media
-        self.output = output
+    def __init__(self, stream: StreamQuery, output):
+        self.__stream = stream
+        self.__output = output
 
     def download(self, itag, filename):
-        self.media.get_by_itag(itag).download(
-            output_path=self.output,
+        self.__stream.get_by_itag(itag).download(
+            output_path=self.__output,
             filename=filename
         )

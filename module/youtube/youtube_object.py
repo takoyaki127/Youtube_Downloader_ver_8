ad81065661a3c223a196cf224eaceef120e91b5f
takoyaki127 <s826062@gmail.com>
2023-05-13 15:02:18 +0900

    メソッド名と可視性を修正

--- module/youtube/youtube_object.py
+++ module/youtube/youtube_object.py
@@ -2,7 +2,6 @@ from pytube import YouTube
 from multiprocessing import Process
 
 from module.youtube.youtube_package.directory import Directory
-from module.youtube.youtube_package.synthesis.synthesis2 import Synthesis
 from module.youtube.youtube_package.list.create_list import CreateList
 
 
@@ -17,38 +16,36 @@ class YoutubeObject(YouTube):
         self.audio_list = cl.audio()
 
     # インデックスを使ってダウンロード
-    def download_with_index(self, video_index, audio_index):
-        self.dir.write_to_settings() # settings.txtを作成
-        self.dir.create_tmp_dir()
+    def __download(self, video_index, audio_index):
+        self.dir.write_settings()    # settings.txtを作成
+        self.dir.create_tmp()       # 一時保存用のフォルダを作成
 
-        self.video = self.video_list.get_video_with_index(video_index)
+        self.video = self.video_list.get_video(video_index)
         self.video.download()
         
-        self.audio = self.audio_list.get_audio_with_index(audio_index)
+        self.audio = self.audio_list.get_audio(audio_index)
         self.audio.download()
 
     # 動画ファイルと音声ファイルを合成
-    def synthesis(self):
+    def __synthesis(self):
         synthesis = self.dir.synthesis_obj()
         synthesis.execute(self.video, self.audio)
 
     # 合成前に使ったファイルを削除
-    def remove(self):
+    def __remove(self):
         self.dir.remove_tmp(self.video)
         self.dir.remove_tmp(self.audio)
 
     # ディスプレイリストをtuple(video_list, audio_list)で返す
     def display_lists_set_to(self,frame):
-        frame.setList(
-            self.video_list.get_display_list(),
-            self.audio_list.get_display_list()
-        )
+        self.video_list.set_list(frame)
+        self.audio_list.set_list(frame)
 
     # ダウンロードから削除までの一通りを行う
     def execute(self, video_index, audio_index):
-        self.download_with_index(video_index, audio_index)
-        self.synthesis()
-        self.remove()
+        self.__download(video_index, audio_index)
+        self.__synthesis()
+        self.__remove()
 
     # ダウンロードから削除までの一通りを行うプロセスを作成
     def prepare_multiprocessing(self,video_index,audio_index):

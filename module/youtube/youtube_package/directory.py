7227e095e08b9e2701583d6145b6fa707447aa57
takoyaki127 <s826062@gmail.com>
2023-05-13 16:13:09 +0900

    変数の可視性を変更

--- module/youtube/youtube_package/directory.py
+++ module/youtube/youtube_package/directory.py
@@ -8,26 +8,26 @@ from module.youtube.youtube_package.synthesis.synthesis import Synthesis
 
 class Directory():
     def __init__(self, download, title):
-        self.download = download
-        self.tmp = self.download + r"\tmp"
-        self.output = self.__create_output(title)
+        self.__download = download
+        self.__tmp = self.__download + r"\tmp"
+        self.__output = self.__create_output(title)
 
     def __create_output(self, title):
         title = Title.escape(title)
-        return self.download + "\\" + title + ".mp4"
+        return self.__download + "\\" + title + ".mp4"
 
     def create_tmp(self):
-        if not os.path.isdir(self.tmp):
-            os.makedirs(self.tmp)
+        if not os.path.isdir(self.__tmp):
+            os.makedirs(self.__tmp)
 
     def write_settings(self):
-        Settings.write_dir(self.download)
+        Settings.write_dir(self.__download)
 
     def get_download_obj(self, stream):
-        return Download(stream, self.tmp)
+        return Download(stream, self.__tmp)
 
     def remove_tmp(self, media: Media):
-        media.remove(self.tmp)
+        media.remove(self.__tmp)
 
     def synthesis_obj(self):
-        return Synthesis(self.tmp, self.output)
+        return Synthesis(self.__tmp, self.__output)

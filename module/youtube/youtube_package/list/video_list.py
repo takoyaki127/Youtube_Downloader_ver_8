1c12f6c533b455c22c30016e78cd2988f3daab89
takoyaki127 <s826062@gmail.com>
2023-05-14 14:19:49 +0900

    format

--- module/youtube/youtube_package/list/video_list.py
+++ module/youtube/youtube_package/list/video_list.py
@@ -13,18 +13,18 @@ class VideoList(MediaList):
             "{:>4}{:>12}{:^30}{:<20}".format(
                 element['itag'],
                 element["qualityLabel"],
-                self.__mimetype_arrange(element['mimeType'],0),
-                self.__mimetype_arrange(element['mimeType'],1)
+                self.__mimetype_arrange(element['mimeType'], 0),
+                self.__mimetype_arrange(element['mimeType'], 1)
             )for element in self.list
         ]
         return display_list
-    
+
     @staticmethod
-    def __mimetype_arrange(mimetype:str, n):
+    def __mimetype_arrange(mimetype: str, n):
         return mimetype.split(';')[n]
 
     def get_element(self, index):
         return Video(self.list[index], self.download)
-    
-    def set_list(self,frame):
+
+    def set_list(self, frame):
         frame.set_video_list(self.display_list)

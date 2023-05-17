0b4e263307525782e0957141b993cf932f888df3
takoyaki127 <s826062@gmail.com>
2023-05-18 00:17:17 +0900

    self.downloadをprivateに変更

--- module/youtube/youtube_package/list/create_list.py
+++ module/youtube/youtube_package/list/create_list.py
@@ -7,14 +7,14 @@ class CreateList():
 
     def __init__(self, data_list, streams, dir: Directory):
         self.__data_list = CreateList.__date_arrange(data_list)
-        self.download = dir.get_download_obj(streams)
+        self.__download = dir.get_download_obj(streams)
 
     @staticmethod
     def __date_arrange(data_list):
         return data_list["adaptiveFormats"]
 
     def video(self):
-        return VideoList(self.__data_list, self.download)
+        return VideoList(self.__data_list, self.__download)
 
     def audio(self):
-        return AudioList(self.__data_list, self.download)
+        return AudioList(self.__data_list, self.__download)

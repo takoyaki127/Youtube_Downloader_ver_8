91b63840f8d2bfa34a18752470209b8c9b17488a
takoyaki127 <s826062@gmail.com>
2023-05-20 01:47:48 +0900

    private

--- module/frame/frame1.py
+++ module/frame/frame1.py
@@ -9,20 +9,17 @@ from module.frame.module.directory_frame import DirectlyFrame
 class Frame1(Frame):
     def __init__(self, root=None, bg=None):
         super().__init__(root, bg)
-        self.create_widget()
+        self.__create_widget()
 
-    def create_widget(self):
+    def __create_widget(self):
         self.btm = BottomFrame(self, '次へ')
         main = MainFrame(self, padx=20)
 
         label_font = ('meiryo', 10)
         entry_font = ('meiryo', 13)
 
-        # ディレクトリ指定のフレーム
-        self.dir_frame = DirectlyFrame(main, label_font, entry_font)
-
-        # URL指定のフレーム
-        self.url_frame = URLFrame(main, label_font, entry_font)
+        self.__dir_frame = DirectlyFrame(main, label_font, entry_font)
+        self.__url_frame = URLFrame(main, label_font, entry_font)
 
     def entry_val(self):
-        return CreateYouTube(self.url_frame, self.dir_frame)
+        return CreateYouTube(self.__url_frame, self.__dir_frame)
